66d170be7b9c33edbaaa135f4fdc1511bc372e81
Cova97 <acovamartinez@gmail.com>
2023-04-21 14:28:25 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+import os
+import openai
+#from dotenv import load_dotenv
+
+#load_dotenv()
+openai.api_key = os.getenv("OPEN_API_KEY")
+
+app = Flask(__name__)
+
+@app.route("/", methods=['POST'])
+def resul():
+    prompt = request.json['prompt']
+    response = openai.Image.create(
+        prompt=prompt,
+        n=1,
+        size="512x512"
+    )
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
