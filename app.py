811ba7de4c6a9cf1922dc46f77853972ae012fb6
Cova97 <acovamartinez@gmail.com>
2023-04-21 18:24:06 -0600

    cors

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, jsonify
 import os
 import openai
+from flask_cors import CORS
 #from dotenv import load_dotenv
 
 #load_dotenv()
 openai.api_key = os.getenv("OPEN_API_KEY")
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/", methods=['POST'])
 def resul():
